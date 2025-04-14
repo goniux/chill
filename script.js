8e5baedf1418655232a1e90d3eb5b853e54308d7
Akshat Srivastva <142911546+goniux@users.noreply.github.com>
2025-04-15 03:14:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Add the click event listener to the button
     document.getElementById("unhackButton").onclick = function() {
-        // Open a new tab or window with the YouTube link
+        // Open a YouTube video in a new tab
         window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
     };
 });
